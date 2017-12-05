123e70c40d4298ae0f991370d511c4669a78358a
jefo <johnyjefo@gmail.com>
2017-12-05 14:49:31 +0300

    :repeat: cleanup

--- src/components/PercentInput.js
+++ src/components/PercentInput.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-// const PercentInput = ({ item, updateValue }) => (
-//     <div>
-//         <input type='range' value={item.percent} onChange={updateValue} />
-//         <input type='number' value={item.percent} />
-//     </div>
-// );
-
 class PercentInput extends React.Component {
 
     constructor(...args) {
@@ -18,14 +11,14 @@ class PercentInput extends React.Component {
         let { item, updateValue } = this.props;
         return  (
             <div key={item.name}>
-                <input name={item.name} type='range' value={item.percent} onChange={this.onValueChange} />
+                <input name={item.name} step='0.01' type='range' value={item.percent} onChange={this.onValueChange} />
                 <input name={item.name} step='0.01' type='number' value={item.percent} onChange={this.onValueChange} />
             </div>
         )
     }
 
     onValueChange(e) {
-        this.props.updateValue(e.target.name, parseFloat(e.target.value));
+        this.props.updateValue(e.target.name, parseInt(e.target.value, 10));
     }
 }
 
--- src/components/PercentInputList.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import PercentInput from './PercentInput';
-
-const PercentInputList = ({ items }) => items.map(item => <PercentPicker item={item} />);
-
-export default PercentInputList;
