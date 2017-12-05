b4173d6440ac0a7f2c0b781f364564d6b3b97ec4
jefo <johnyjefo@gmail.com>
2017-12-05 14:37:46 +0300

    :repeat: rm logger

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import percents from './percents';
 
-const store = createStore(percents, applyMiddleware(createLogger()));
+const store = createStore(percents);
 
 export default store;
